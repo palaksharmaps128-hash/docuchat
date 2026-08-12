9415345ce898d2f4d711105af9abddd176c6f92f
palaksharmaps128-hash <palaksharmaps128@gmail.com>
2026-08-12 10:09:10 +0530

    Enable threading for real speed fix

--- server.py
+++ server.py
@@ -92,4 +92,4 @@ def ask_endpoint():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='0.0.0.0', threaded=True)
